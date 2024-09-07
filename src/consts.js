5dec734aab1888ad5a75d1a5f50c3c3e61e0378a
Omar Ahmed <0omarahmed73@gmail.com>
2024-09-07 07:49:14 +0300

    first commit

---
+++ src/consts.js
@@ -0,0 +1,130 @@
+export const letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export const defaultLetterClass = 'letter border-2 text-black w-12 h-12  flex items-center justify-center rounded-md cursor-pointer select-none hover:scale-110 transition ease-in-out'
+
+
+export const words = [
+  {
+    word: 'Banana',
+    hint: 'A yellow fruit that monkeys love.'
+  },
+  {
+    word: 'Elephant',
+    hint: 'A large mammal with a trunk.'
+  },
+  {
+    word: 'Pyramid',
+    hint: 'An ancient structure built by the Egyptians.'
+  },
+  {
+    word: 'Cairo',
+    hint: 'The capital of Egypt, home to the Great Pyramids.'
+  },
+  {
+    word: 'Riyadh',
+    hint: 'The capital city of Saudi Arabia.'
+  },
+  {
+    word: 'Amman',
+    hint: 'The capital of Jordan, known for its ancient ruins.'
+  },
+  {
+    word: 'Baghdad',
+    hint: 'The capital of Iraq, once the center of the Abbasid Caliphate.'
+  },
+  {
+    word: 'Muscat',
+    hint: 'The capital of Oman, known for its stunning coastline.'
+  },
+  {
+    word: 'Mecca',
+    hint: 'The holiest city in Islam, birthplace of the Prophet Muhammad.'
+  },
+  {
+    word: 'Quran',
+    hint: 'The holy book of Islam.'
+  },
+  {
+    word: 'Ramadan',
+    hint: 'The Islamic month of fasting from dawn until sunset.'
+  },
+  {
+    word: 'Zakat',
+    hint: 'An obligatory form of charity in Islam.'
+  },
+  {
+    word: 'Hijra',
+    hint: 'The migration of Prophet Muhammad from Mecca to Medina.'
+  },
+  {
+    word: 'Hajj',
+    hint: 'The annual pilgrimage to Mecca, one of the Five Pillars of Islam.'
+  },
+  {
+    word: 'Salah',
+    hint: 'The ritual prayer performed five times a day in Islam.'
+  },
+  {
+    word: 'Umrah',
+    hint: 'A pilgrimage to Mecca that can be done at any time of the year.'
+  },
+  {
+    word: 'Halal',
+    hint: 'Permissible actions or food according to Islamic law.'
+  },
+  {
+    word: 'Sunnah',
+    hint: 'The practices and teachings of the Prophet Muhammad.'
+  },
+  {
+    word: 'Sharia',
+    hint: 'Islamic law derived from the Quran and Hadith.'
+  },
+  {
+    word: 'Fajr',
+    hint: 'The first prayer of the day, performed at dawn.'
+  },
+  {
+    word: 'Tawhid',
+    hint: 'The oneness of God in Islam.'
+  },
+  {
+    word: 'Eid',
+    hint: 'Islamic festivals, including Eid al-Fitr and Eid al-Adha.'
+  },
+  {
+    word: 'Imam',
+    hint: 'A leader of prayer in a mosque.'
+  },
+  {
+    word: 'Sawm',
+    hint: 'Fasting during the month of Ramadan, one of the Five Pillars of Islam.'
+  }
+];
